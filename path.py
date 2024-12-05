92d25096eab9d30cc3dd50083e9c7a64e43e7017
Dylan Greene <dylantgreene92@gmail.com>
2024-12-05 00:04:09 -0600

    Update path.py

--- path.py
+++ path.py
@@ -6,15 +6,33 @@ from mathutils import Vector
 from enum import Enum
 
 precision = 4
+
+
+# Auto-fit scales and centers the image within the target dimensions including margins
+# Auto-fit settings
+enable_auto_fit = True
 target_width = 256
 target_height = 256
 x_margin = 10
 y_margin = 10
-output_path = 'C:\\Users\\Dylan\\Downloads\\monsterra.svg'
 
-CommandType = Enum('CommandType', ['MoveTo', 'CurveTo', 'ClosePath'])
+# Absolute output path
+output_path = 'C:\\Users\\Dylan\\Downloads\\blender_output.svg'
 
-class MoveTo:
+CommandType = Enum('CommandType', ['MoveTo', 'CurveTo', 'ClosePath', 'LineTo'])
+
+class BaseCommand:
+    def scale(self, factor):
+        pass
+
+    def translate(self, offset):
+        pass
+
+    def to_d(self):
+        raise NotImplementedError('to_d() has not been implemented for this command.')
+    
+
+class MoveTo(BaseCommand):
     command_type = CommandType.MoveTo
     pos = None
     
@@ -32,7 +50,26 @@ class MoveTo:
         
         return f'M{pos[0]},{pos[1]}'
 
-class CurveTo:
+class LineTo(BaseCommand):
+    command_type = CommandType.LineTo
+    pos = None
+    
+    def __init__(self, pos):
+        self.pos = pos
+        
+    def scale(self, factor):
+        self.pos = self.pos * factor
+    
+    def translate(self, offset):
+        self.pos = self.pos + offset
+    
+    def to_d(self):
+        pos = self.pos.to_tuple(precision)
+        
+        return f'L{pos[0]},{pos[1]}'
+
+
+class CurveTo(BaseCommand):
     command_type = CommandType.CurveTo
     pos = None
     control_1 = None
@@ -60,7 +97,7 @@ class CurveTo:
         
         return f'C{control_1[0]},{control_1[1]} {control_2[0]},{control_2[1]} {pos[0]},{pos[1]}'
 
-class ClosePath:
+class ClosePath(BaseCommand):
     command_type = CommandType.ClosePath
     def to_d(self):
         return 'Z'
@@ -88,59 +125,43 @@ def main():
             continue
         
         if obj.data.dimensions != '2D':
-            raise ValueError('Selected curve(s) are using the incorrect shape.  Shape must be set to 2D.')
+            raise ValueError('Selected curve(s) are using the incorrect curve type.  The curve type must be set to 2D.')
         
         obj_scale = obj.scale.to_2d()
+        scale_diff = abs(obj_scale[0] - obj_scale[1])
         
-        if obj.rotation_euler.z != 0 or obj.location.to_2d().length > 0 or obj_scale[0] != 1 or obj_scale[1] != 1:
+        if obj.rotation_euler.z != 0 or obj.location.to_2d().length > 0 or scale_diff > 0.01:
             raise ValueError('Selected curve(s) have un-applied transformations.  Please apply these first.')
 
         for spline in obj.data.splines:
-            path = []
-            prev_handle_2 = None
-            
-            for point in spline.bezier_points:
-                control_point = point.co.to_2d()
-                handle_1 = point.handle_left.to_2d()
-                handle_2 = point.handle_right.to_2d()
-                
-                if not path:
-                    path.append(MoveTo(control_point))
-                else:
-                    path.append(CurveTo(control_point, prev_handle_2, handle_1))
+            match spline.type:
+                case 'POLY':
+                    all_paths.append(convert_poly(spline))
+                case 'BEZIER':
+                    all_paths.append(convert_bezier(spline))
+                case _:
+                    raise ValueError('NURBS spline type is not currently supported.')
 
-                prev_handle_2 = handle_2
-                
-            if spline.use_cyclic_u:
-                # Curve to the beginning point when closing
-                first_point = spline.bezier_points[0]
-                first_control_point = first_point.co.to_2d()
-                first_handle_1 = first_point.handle_left.to_2d()
-                
-                path.append(CurveTo(first_control_point, prev_handle_2, first_handle_1))
-                path.append(ClosePath())
-            
-            all_paths.append(path)
+    # Convert from meters to millimeters
+    for path in all_paths:
+        for command in path:
+            command.scale(1000.0)
     
     # Calculate the bounding box for the commands
     bounds = Bounds()
     
     for path in all_paths:
         for command in path:
-            if command.command_type == CommandType.MoveTo or command.command_type == CommandType.CurveTo:
+            if command.command_type != CommandType.ClosePath:
                 bounds.min_x = min(command.pos[0], bounds.min_x)
                 bounds.max_x = max(command.pos[0], bounds.max_x)
                 bounds.min_y = min(command.pos[1], bounds.min_y)
                 bounds.max_y = max(command.pos[1], bounds.max_y)
 
-    input_width = abs(bounds.max_x) - abs(bounds.min_x)
-    input_height = abs(bounds.max_y) - abs(bounds.min_y)
+    input_width = bounds.max_x - bounds.min_x
+    input_height = bounds.max_y - bounds.min_y
     
-    output_width = target_width - x_margin * 2.0
-    output_height = target_height - y_margin * 2.0
     
-    # Determine the scale needed to reach the target dimensions
-    scale_factor = min(output_width / input_width, output_height / input_height)
     
     # Offset the points based on the bounds minimums. This makes the points essentially start at (0, 0).
     reset_offset = Vector((-bounds.min_x, -bounds.min_y))
@@ -148,6 +169,45 @@ def main():
     flip_factor = Vector((1.0, -1.0))
     # After flipping, the points need to be offset by the height to bring them back into view.
     flip_offset = Vector((0.0, input_height))
+
+    # Perform base scale and alignment
+    for path in all_paths:
+        for command in path:
+            if command.command_type != CommandType.ClosePath:
+                command.translate(reset_offset)
+                command.scale(flip_factor)
+                command.translate(flip_offset)
+    
+    # Perform auto-alignment if enabled
+    if enable_auto_fit:
+        auto_fit(all_paths, input_width, input_height)
+    
+    # Accumulate path data
+    for path in all_paths:
+        path_data = []
+        for command in path:
+            path_data.append(command.to_d())
+        
+        path_element = ElementTree.Element('path')
+        path_element.set('d', ' '.join(path_data))
+        svg.append(path_element)
+
+    if enable_auto_fit:
+        svg.set('viewBox', f'0 0 {target_width}mm {target_height}mm')
+    else:
+        svg.set('viewBox', f'0 0 {round(input_width, precision)}mm {round(input_height, precision)}mm')
+    
+    f = open(output_path, 'w')
+    f.write(pretty_xml(svg))
+    f.close()
+
+def auto_fit(all_paths, input_width, input_height):
+    output_width = target_width - x_margin * 2.0
+    output_height = target_height - y_margin * 2.0
+    
+    # Determine the scale needed to reach the target dimensions
+    scale_factor = min(output_width / input_width, output_height / input_height)
+    
     # The dimensions already factored in 2x the margins, here we just need to move the points over by the margin values.
     margin_offset = Vector((x_margin, y_margin))
     # Aligns the points to the middle
@@ -155,31 +215,57 @@ def main():
         (output_width - input_width * scale_factor) * 0.5, 
         (output_height - input_height * scale_factor) * 0.5
     ))
-
-    # Scale and accumulate commands
+    
     for path in all_paths:
-        path_data = []
         for command in path:
-            if command.command_type == CommandType.MoveTo or command.command_type == CommandType.CurveTo:
-                command.translate(reset_offset)
-                command.scale(flip_factor)
-                command.translate(flip_offset)
+            if command.command_type != CommandType.ClosePath:
                 command.scale(scale_factor)
                 command.translate(margin_offset)
                 command.translate(align_offset)
-            
-            path_data.append(command.to_d())
-        
-        path_element = ElementTree.Element('path')
-        path_element.set('d', ' '.join(path_data))
-        svg.append(path_element)
 
-    svg.set('viewBox', f'0 0 {target_width} {target_height}')
+def convert_poly(spline):
+    path = []
     
-    f = open(output_path, 'w')
-    f.write(pretty_xml(svg))
-    f.close()
+    for i, point in enumerate(spline.points):
+        pos = point.co.to_2d()
+        
+        if i == 0:
+            path.append(MoveTo(pos))
+        else:
+            path.append(LineTo(pos))
     
+    if spline.use_cyclic_u:
+        path.append(ClosePath())
+    
+    return path
+
+def convert_bezier(spline):
+    path = []
+    prev_handle_2 = None
+    
+    for point in spline.bezier_points:
+        control_point = point.co.to_2d()
+        handle_1 = point.handle_left.to_2d()
+        handle_2 = point.handle_right.to_2d()
+        
+        if not path:
+            path.append(MoveTo(control_point))
+        else:
+            path.append(CurveTo(control_point, prev_handle_2, handle_1))
+
+        prev_handle_2 = handle_2
+        
+    if spline.use_cyclic_u:
+        # Curve to the beginning point when closing
+        first_point = spline.bezier_points[0]
+        first_control_point = first_point.co.to_2d()
+        first_handle_1 = first_point.handle_left.to_2d()
+        
+        path.append(CurveTo(first_control_point, prev_handle_2, first_handle_1))
+        path.append(ClosePath())
+    
+    return path
+
 
 def pretty_xml(elem):
     """Returns a pretty-printed XML string for the Element"""
